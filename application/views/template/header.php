8ac7a4752306115c9112254ca44384250eeccade
Ronald Arrival Fajar <38059376+juraganpisang@users.noreply.github.com>
2022-06-17 00:05:45 +0700

    [UPD] Icon

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <title>Geprec Web | <?= $title; ?></title>
     <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-    <link rel="icon" href="<?= config_item('img') ?>icon.ico" type="image/x-icon" />
+    <link rel="icon" href="<?= config_item('img') ?>GEPREC-01.ico" type="image/x-icon" />
 
     <!-- CSS Files -->
     <link rel="stylesheet" href="<?= config_item('css') ?>bootstrap.min.css">
